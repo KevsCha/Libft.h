b9970b303044821288d085679f8b370155678a77
Kevin Quispe Carrillo <107918168+KevsCha@users.noreply.github.com>
2023-10-06 20:46:23 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -1,5 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kquispe <kquispe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 17:45:08 by kquispe           #+#    #+#             */
+/*   Updated: 2023/10/06 20:35:34 by kquispe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//s = " hola  como estas";
+// c = " ";
+
 char	**ft_split(char const *s, char c)
 {
+	int		i;
+	size_t	j;
+	char	**arrStr;
 
+	i = 0;
+	j = 1;
+	//con esto calculo el leng de las palabras
+	while (s[i])
+	{
+		if (s[i] != c && s[i + 1] == c)
+		{
+			printf("%d\n", i);
+			j++;
+		}
+		else if (s[i] == c && s[i + 1] == '\0')
+			j--;
+		i++;
+	}
+	
+	
+	//TODO:Contar palabras  dependiendo del caracter pasado en  "c"
+	//HAcer un malloc con el iterardor contado
+	//utilizar
+	// str = (char **)malloc
+	// str[i] = ft_substr
+	 printf("len _ %ld\n%d", j, i);
 	return (0);
 }
--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include <string.h>
 #include "libft.h"
-
+#include <ctype.h>
 int main(void)
 {
-	char *str1 = "... hhola que tal ...";
-	char *set = ". h";
-	
-	printf("%s", ft_strtrim(str1, set));
+	char *str1 = " hdo lahhh ";
+	char c = ' ';
+	char **strs;
+	ft_split(str1, c);
 	/*
+	1 = "    h"
+	2 = " la c "
+	3 = " m "
+	4 = " estas fsd "
+	5 = " "
+	6 = " "
+	*/
+
+	
+	/*
+	*fT_split = :C
 	*ft_strjoin = :3
 	*ft_strnstr = :3
 	*ft_memcmp = :3
+	+ft_substr = :3 
 	*/
 	return (0);
 }
