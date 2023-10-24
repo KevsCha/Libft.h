652a29b50cbea7c5ee33a7fc75175be48869db37
KevsCha <k-e-v-i-n_13_@hotmail.com>
2023-10-24 10:32:37 +0200

    Error Solucionado

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kquispe <kquispe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:28:33 by kquispe           #+#    #+#             */
-/*   Updated: 2023/10/17 19:59:13 by kquispe          ###   ########.fr       */
+/*   Updated: 2023/10/21 03:54:25 by kquispe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,19 @@
 char	*ft_strnstr(const char *str1, const char *str2, size_t num)
 {
 	size_t	i;
+	size_t	n;
 
+	i = 0;
 	if (!str2[0])
 		return ((char *)str1);
-	if (!num)
-		return (0);
-	while (*str1 && --num)
+	while (str1[i] && i < num)
 	{
-		i = 0;
-		while (str1[i] == str2[i] && i < num)
-		{	
-			if (str2[i + 1] == '\0')
-				return (str1);
-			i++;
-		}
-		if (str1 != '\0' && i == num)
-			return (str1);
-		str1++;
+		n = 0;
+		while (str1[i + n] == str2[n] && str1[i + n] && (i + n) < num)
+			n++;
+		if (str2[n] == '\0')
+			return ((char *)str1 + i);
+		i++;
 	}
 	return (0);
 }
