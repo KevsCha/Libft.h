a0c167f995f0bb34f2df113a45d4d4b5446efca6
KevsCha <k-e-v-i-n_13_@hotmail.com>
2023-10-24 16:17:46 +0200

    error Solucionado

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,13 @@
 /*   By: kquispe <kquispe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 17:45:08 by kquispe           #+#    #+#             */
-/*   Updated: 2023/10/17 17:02:29 by kquispe          ###   ########.fr       */
+/*   Updated: 2023/10/24 16:13:40 by kquispe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//?str = lorem ipsum..., posig = i(0,1,2,3,4,5..),c = " " 
 unsigned int	ft_start(const char *str, int posig, char c)
 {
 	int	i;
@@ -19,12 +20,19 @@ unsigned int	ft_start(const char *str, int posig, char c)
 
 	i = 0;
 	start = 0;
+	if (posig == 0 && str[0] != c)
+		return(0);	
 	while (str[i])
 	{
-		if (str[i] == c && start == posig)
-			break ;
-		if (str[i] == c)
+		if (i == 0)
+			while (str[i] == c)
+				i++;
+		if (str[i] == c && str[i + 1] != c)
 			start++;
+		if (posig == 0)
+			i--;
+		if (start == posig)
+			break;
 		i++;
 	}
 	i++;
@@ -36,7 +44,7 @@ size_t	ft_end(const char *str, int posin, char c)
 	size_t	i;
 
 	i = 0;
-	while (str[posin] != c)
+	while (str[posin] != c && str[posin] != '\0')
 	{
 		i++;
 		posin++;
@@ -64,19 +72,22 @@ char	**ft_split(char const *s, char c)
 {
 	size_t	leng_astr;
 	char	**astr;
+	int		start;
+	int		end;
 	int		i;
-	int		j;
 
 	i = 0;
 	leng_astr = ft_leng_malloc(s, c) + 1;
 	astr = (char **)malloc((leng_astr) * sizeof(char *));
 	if (astr == NULL)
 		return (NULL);
-	astr[leng_astr] = NULL;
+	astr[leng_astr - 1] = NULL;
+	
 	while (leng_astr > 1)
 	{
-		j = ft_start(s, i, c);
-		astr[i] = ft_substr(s, j, ft_end(s, j, c));
+		start = ft_start(s, i, c);
+		end = ft_end(s, start, c);
+		astr[i] = ft_substr(s, start, end);
 		i++;
 		leng_astr--;
 	}
